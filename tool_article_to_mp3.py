a280b680e1c6c14914764fe338518e45473c21bd
lichanping <lichanping@users.noreply.github.com>
2024-04-06 14:47:16 +0000

    deploy: 87dd0e295737d695bc03f40ae6508573e202e4ae

--- tool_article_to_mp3.py
+++ tool_article_to_mp3.py
@@ -13,35 +13,47 @@ class TextToSpeechConverter:
     def __init__(self):
         pass
 
-    async def read_text_from_file(self, text_file_name):
-        file_path = os.path.join(get_sub_folder_path(), text_file_name)
-        with open(file_path, 'r', encoding='utf-8') as file:
+    async def read_text_files(self, input_folder):
+        # Get the path of the input folder
+        input_folder_path = os.path.join(get_sub_folder_path(), input_folder)
+        # Get the list of all text files in the input folder
+        text_files = [file for file in os.listdir(input_folder_path) if file.endswith('.txt')]
+        return text_files
+
+    async def read_text_from_file(self, text_file_path):
+        with open(text_file_path, 'r', encoding='utf-8') as file:
             # Read the entire content of the file
             return file.read().strip()  # Remove leading/trailing whitespace
 
-    async def process_text_file(self, text_file_name, output_folder):
-        text_content = await self.read_text_from_file(text_file_name)
+    async def process_text_file(self, text_file_name, input_folder, output_folder):
+        # Create the input file path
+        input_file_path = os.path.join(get_sub_folder_path(), input_folder, text_file_name)
+        text_content = await self.read_text_from_file(input_file_path)
 
         # Create output file path
         output_file_name = text_file_name.replace(".txt", "") + ".mp3"
         output_file = os.path.join(output_folder, output_file_name)
+
         # Create VoicesManager instance
         voices = await VoicesManager.create()
         voice = voices.find(Gender="Female", Language="en")
-        communicate = edge_tts.Communicate(text_content, random.choice(voice)["Name"])
+        communicate = edge_tts.Communicate(text_content, random.choice(voice)["Name"], rate="-10%")
         await communicate.save(output_file)
 
-    async def convert_text_to_speech(self, text_file_names, output_folder):
+    async def convert_text_to_speech(self, input_folder, output_folder):
         start_time = time.time()
-        for text_file_name in text_file_names:
-            await self.process_text_file(text_file_name, output_folder)
+        # Read all text files from the input folder
+        text_files = await self.read_text_files(input_folder)
+        # Process each text file
+        for text_file_name in text_files:
+            await self.process_text_file(text_file_name, input_folder, output_folder)
         end_time = time.time()  # Record end time
         elapsed_time = end_time - start_time  # Calculate elapsed time
         print(f"Total elapsed time: {elapsed_time:.2f} seconds")
 
 
 if __name__ == "__main__":
-    text_file_names = ["writing1.txt", "writing2.txt"]  # Specify the text file names here as a list
+    input_folder = "articles"  # Specify the input folder containing text files
     output_folder = "article_to_mp3"  # Specify the output folder here
     output_folder_path = os.path.join(get_sub_folder_path(), output_folder)
 
@@ -49,4 +61,4 @@ if __name__ == "__main__":
     os.makedirs(output_folder_path, exist_ok=True)
 
     converter = TextToSpeechConverter()
-    asyncio.run(converter.convert_text_to_speech(text_file_names, output_folder_path))
+    asyncio.run(converter.convert_text_to_speech(input_folder, output_folder_path))
