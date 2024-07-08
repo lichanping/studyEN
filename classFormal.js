b9c7b4b11c99bcb385fdf7b7b2cf29c7f48299a7
shirley <shirley.li@lego.com>
2024-07-08 13:51:50 +0800

    more

--- classFormal.js
+++ classFormal.js
@@ -139,9 +139,9 @@ export function handleScheduleNotificationClick() {
     let notificationMessage;
     if (timeDifference > 0 && timeDifference <= 30) {
         // Calculate the countdown time
-        notificationMessage = `【词汇课-${thisDateTime}】<br><br>⏰我们的在线课程还有【${timeDifference}】分钟开始了，请做好准备，及时进入会议室哦🔥`;
+        notificationMessage = `【${thisDateTime}】<br><br>⏰我们的在线课程还有【${timeDifference}】分钟开始了，请做好准备，及时进入会议室哦🔥`;
     } else {
-        notificationMessage = `【词汇课-${thisDateTime}】<br><br>亲爱的 ✨ ${userName} 用户您好! 我们的语言陪练服务时间安排在${formattedDateTime}。<br><br>📢请学员及家长准时进入会议室上课，并确保摄像头📷开启。感谢您的配合！`;
+        notificationMessage = `【${thisDateTime}】<br><br>亲爱的 ✨ ${userName} 用户您好! 我们的语言陪练服务时间安排在${formattedDateTime}。<br><br>📢请学员及家长准时进入会议室上课，并确保摄像头📷开启。感谢您的配合！`;
     }
     copyToClipboard(notificationMessage);
     showLongText(`${notificationMessage}`);
