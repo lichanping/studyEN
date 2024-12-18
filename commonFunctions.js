02e16de5b326f6d0101ffb3f90520cefbbbbe015
lichanping <lichanping@users.noreply.github.com>
2024-12-18 23:32:41 +0000

    deploy: 7ec9337f5d1998c2029320c7bcc511349f16316a

--- commonFunctions.js
+++ commonFunctions.js
@@ -163,7 +163,7 @@ export function handleManagementGroupTemplateClick() {
     if (timeDifference > 0 && timeDifference <= 30) {
         outputMessage = `【抗遗忘提醒-${hour}:${minute}】<br><br>⏰我们的抗遗忘复习课还有 **${timeDifference}** 分钟开始了，请做好准备，及时进入会议室哦🔥`;
     } else {
-        outputMessage = `【抗遗忘温馨提醒-${hour}:${minute}】<br><br>👍 #腾讯会议：988-8340-0582<br><br>⏰ 请准时参加会议，如有特殊情况无法参加，请提前告知！<br><br>🔔 重要提醒：若未按约定时间参加复习，请自主复习并打卡至学习群📱<br><br>🔒 仅在当天有收到提醒通知才需入会。<br><br>💝 在求知的旅程中，你的每一分努力老师都铭记于心，老师为你骄傲！`;
+        outputMessage = `【抗遗忘温馨提醒-${hour}:${minute}】<br><br>👍 #腾讯会议：988-8340-0582<br><br>⏰ 请准时参加会议，如有特殊情况无法参加，请提前告知！<br><br>🔔 若未按约定时间参加复习，请自主复习并打卡至学习群📱<br><br>🔒 仅在当天有收到提醒通知才需入会。<br><br>📢 请在抗遗忘时及时反馈是否需要取消下次安排的正课，以便妥善安排教学计划，老师为你骄傲🌹`;
     }
 
     copyToClipboard(outputMessage);
