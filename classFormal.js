f6d0139afc2787ae0a2bcabb3efa1258334bb8a3
lichanping <lichanping@users.noreply.github.com>
2025-03-07 09:11:35 +0000

    deploy: d078736d8c9ac049a1c261df14d6bf881be7259b

--- classFormal.js
+++ classFormal.js
@@ -57,9 +57,9 @@ const teacherData = {
             },
             "泽瑞": {
                 schedule: "每周五晚上+周日16点 泽瑞 (初三)",
-                course: "中考超纲",
+                course: "2025年上海中考超纲词汇",
                 hours: [19, 40],
-                courseWordCount: 272
+                courseWordCount: 2720
             },
             "泽瑞同学": {
                 schedule: "仅用于核对工资，不用于统计抗遗忘复习 泽瑞同学 (初三)",
