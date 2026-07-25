4e2e8e28cffe78d81004e5d6a04cae8558a298b0
cnShirLi <shirley.li@lego.com>
2026-07-25 13:19:36 +0800

    feat: refine baifendii group greeting

--- tests/test-formal-group-greeting-baifendii.js
+++ tests/test-formal-group-greeting-baifendii.js
@@ -29,4 +29,15 @@ assert(
     '百分缔入群招呼语应包含教师身份说明'
 );
 
+assert(
+    content.includes('温馨提示：<br>1.请提前给孩子准备一个网络信号好、安静的环境，以确保良好的学习体验。<br>2.课中孩子大量跟读，提前准备一杯温开水'),
+    '百分缔入群招呼语应包含独立的温馨提示段落'
+);
+
+assert(
+    content.includes('请您关注！<br><br>温馨提示：')
+        && content.includes('温开水<br><br>这是我的个人介绍'),
+    '百分缔入群招呼语应在个人介绍前插入温馨提示段落，并与前后段落各保留一个空白行'
+);
+
 console.log('test-formal-group-greeting-baifendii passed');
